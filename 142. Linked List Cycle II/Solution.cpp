a052ec0557e71c25768b38e725b5a4e532e8d4b6
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-02-26 21:33:28 +0530

    Update Solution.cpp

--- 142. Linked List Cycle II/Solution.cpp
+++ 142. Linked List Cycle II/Solution.cpp
@@ -8,6 +8,8 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//Solution using hash map
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
@@ -22,5 +24,36 @@ public:
             start=start->next;
         }
         return NULL;
+    }
+};
+//solution using two pointer
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(slow && fast && fast->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast)
+                {
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }
+        }
+   return NULL;
     }
 };
