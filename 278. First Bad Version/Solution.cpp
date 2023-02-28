14bcebc6d11771b38371cecd4f541b5577d814e4
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-02-28 12:18:11 +0530

    Create Solution.cpp

---
+++ 278. First Bad Version/Solution.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/first-bad-version/description/?envType=study-plan&id=level-1
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long int left=1;
+        long int right = n;
+        long int mid;
+        while(n>1)
+        {
+            mid=(left+right)/2;
+            if(isBadVersion(mid)) 
+            {
+                right=mid-1;
+            }
+            else
+            {
+                left=mid+1;
+                
+            }
+            if(isBadVersion(mid) && !isBadVersion(mid-1)) return mid;
+        }
+        return 1;
+    }
+};
