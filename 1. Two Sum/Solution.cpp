277099ad281823ab1c28bd02ee08a16b82feb5a0
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-03-07 20:11:22 +0530

    Create Solution.cpp

---
+++ 1. Two Sum/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    vector<int> twoSum(vector<int> nums, int target) {
+        vector<int>result;
+        map<int,int>mymap;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mymap.find(nums[i])==mymap.end())
+            {
+                mymap[nums[i]]=i;
+            }
+            if(mymap.find(target-nums[i])!=mymap.end() && i!=mymap[target-nums[i]])
+            {
+                result.push_back(i);
+                result.push_back(mymap[target-nums[i]]);
+                break;
+            }
+        }
+        return result;
+        
+    }
+};
